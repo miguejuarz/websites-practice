8f4ccaff5514cc7b4fd7ac24c8029fea62ddfbb6
miguemendoza <125337855+miguejuarz@users.noreply.github.com>
2023-11-18 21:12:53 -0600

    changes add gsap, but still no working xd

---
+++ html-css-gsap-animated-website/script.js
@@ -0,0 +1,36 @@
+TweenMax.staggerForm(".logo", 1.6, {
+    delay: 0.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+TweenMax.staggerForm(".search", 1.6, {
+    delay: 0.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+TweenMax.staggerForm(".bottom-bar", 2, {
+    delay: 0.7,
+    opacity: 0,
+    x: -40,
+    ease: Expo.easeInOut
+});
+TweenMax.staggerForm(".bi", 2, {
+    delay: 2.2,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+});
+TweenMax.staggerForm(".title span", 3, {
+    delay: 0.2,
+    opacity: 0,
+    x: -100,
+    ease: Expo.easeInOut
+});
+TweenMax.staggerForm(".media ul li", 3, {
+    delay: 3.5,
+    opacity: 0,
+    y: 50,
+    ease: Expo.easeInOut
+});
